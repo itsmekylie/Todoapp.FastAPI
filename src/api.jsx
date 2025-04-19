a5ecf19d9c6c6a0b1016059790b26995fb6dfae9
itsmekylie <122257319+itsmekylie@users.noreply.github.com>
2025-04-19 23:31:42 +0800

    jhgnn

--- src/api.jsx
+++ src/api.jsx
@@ -1,4 +1,4 @@
-const API_URL = import.meta.env.VITE_API_URL || "http://127.0.0.1:8000/";
+const API_URL = import.meta.env.VITE_API_URL || "https://fastapi-todoapp-hu2b.onrender.com/";
 
 export const getTodos = async (completed) => {
   let url = `${API_URL}/todos`;
