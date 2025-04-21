3ca7a976138e670ab0e19301b37f64098706c65d
itsmekylie <122257319+itsmekylie@users.noreply.github.com>
2025-04-21 19:11:19 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default {
-  base: "/Todoapp.FastAPI/", // <- must match your repo name!
-};
+export default defineConfig({
+  plugins: [react()],
+  base: "/todo_fastapi/",
+})
