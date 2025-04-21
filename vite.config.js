31dd11cc0cbc46483b5f273d74325dea2e8818ea
itsmekylie <122257319+itsmekylie@users.noreply.github.com>
2025-04-21 19:28:34 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/Todoapp.FastAPI/',  // 👈 important for GitHub Pages
   plugins: [react()],
-  base: "/todo_fastapi/",
 })
